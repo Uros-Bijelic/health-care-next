aaabba5581b404477ee4ccf5d1e0de9179a563a5
urke90 <urosbijelic90@gmail.com>
2025-02-07 15:32:13 +0100

    feat: add div wrapper for heading on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 const Home = () => {
-  return <h1>This is new Tittle app for testing commitlint</h1>;
+  return (
+    <div>
+      <h1>This is new Tittle app for testing commitlint</h1>;
+    </div>
+  );
 };
 
 export default Home;
