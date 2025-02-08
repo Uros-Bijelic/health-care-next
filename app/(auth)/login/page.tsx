930a1ffc9cbe08eeb676d30667b8789cd158a2d2
urke90 <urosbijelic90@gmail.com>
2025-02-08 16:04:16 +0100

    feat: working on Regsiter.txs page and restructure utitltiy classes in globals.css

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,4 +1,8 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
 import RHFInput from '@/components/ui/RHFInputs/RHFInput';
+import Link from 'next/link';
 import { FormProvider, useForm } from 'react-hook-form';
 
 interface ILoginProps {}
@@ -15,10 +19,20 @@ const Login: React.FC<ILoginProps> = () => {
     <div className="flex flex-col gap-3 shadow-xl bg-white w-[min(400px,100%)] p-2 sm:p-5">
       <h2 className="h2-bold text-center">Login</h2>
       <FormProvider {...methods}>
-        <RHFInput name="email" type="email" label="Your Email" placeholder="Email" />
-        <RHFInput name="password" type="password" label="Your Password" placeholder="Password" />
+        <form action="" className="flex flex-col gap-3">
+          <RHFInput name="email" type="email" label="Your Email" placeholder="Email" />
+          <RHFInput name="password" type="password" label="Your Password" placeholder="Password" />
+          <Button type="submit">Login</Button>
+        </form>
+        <div className="flex flex-col gap-2 text-center ">
+          <p className="p2-medium text-center w-full">
+            Or if you don&apos;t have account yet create one
+          </p>
+          <Link href="/register" className="text-blue-500 underline">
+            Sign Up
+          </Link>
+        </div>
       </FormProvider>
-      This is new pagt
     </div>
   );
 };
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,11 +1,39 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
+import RHFInput from '@/components/ui/RHFInputs/RHFInput';
+import Link from 'next/link';
+import { FormProvider, useForm } from 'react-hook-form';
 
 interface IRegisterProps {}
 
-const Register = () => {
+const Register: React.FC<IRegisterProps> = () => {
+  const methods = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
   return (
-    <div>
-      <Button>dsaadssa</Button>
+    <div className="flex flex-col gap-3 shadow-xl bg-white w-[min(400px,100%)] p-2 sm:p-5">
+      <h2 className="h2-bold text-center">Sign Up</h2>
+      <FormProvider {...methods}>
+        <form action="" className="flex flex-col gap-3">
+          <RHFInput name="userName" type="text" label="Your Username" placeholder="UserName" />
+          <RHFInput name="email" type="email" label="Your Email" placeholder="Email" />
+          <RHFInput name="password" type="password" label="Your Password" placeholder="Password" />
+          <Button type="submit">Login</Button>
+        </form>
+        <div className="flex flex-col gap-2 text-center ">
+          <p className="p2-medium text-center w-full">
+            Or if you don&apos;t have account yet create one
+          </p>
+          <Link href="/login" className="text-blue-500 underline">
+            Sign Up
+          </Link>
+        </div>
+      </FormProvider>
     </div>
   );
 };
