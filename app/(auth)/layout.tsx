cbd7ec4b6c214c94d696737116afae28acf09ee9
urke90 <urosbijelic90@gmail.com>
2025-02-08 14:31:22 +0100

    fix: css tweaks to input

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ interface ILayoutProps {
 }
 
 const Layout: React.FC<ILayoutProps> = ({ children }) => {
-  return <section className="h-full border-2 border-cyan-500">{children}</section>;
+  return <section className="h-screen flex-center">{children}</section>;
 };
 
 export default Layout;
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,14 @@
+import { Input } from '@/components/ui/input';
+
 interface ILoginProps {}
 
 const Login: React.FC<ILoginProps> = () => {
-  return <div>Login</div>;
+  return (
+    <div className="flex flex-col gap-3 shadow-xl bg-white w-[min(400px,100%)] p-2 sm:p-5">
+      <Input />
+      This is new pagt
+    </div>
+  );
 };
 
 export default Login;
