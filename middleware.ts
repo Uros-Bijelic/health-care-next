166e8a3f2543a6c259dc52147525e7f7d454d18b
urke90 <urosbijelic90@gmail.com>
2025-03-16 19:39:07 +0100

    feat: setup base route protecting for authenticated user

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,26 @@
 import NextAuth from 'next-auth';
+import { NextResponse } from 'next/server';
 import { AUTH_CONFIG } from './auth.config';
 
-export const { auth: middleware } = NextAuth(AUTH_CONFIG);
+const { auth } = NextAuth(AUTH_CONFIG);
+
+const AUTH_ROUTES = ['/login', '/register'];
+
+export default auth((req) => {
+  const isAuth = !!req.auth;
+  const pathName = req.nextUrl.pathname;
+
+  if (!isAuth) {
+    if (!AUTH_ROUTES.includes(pathName)) {
+      return NextResponse.redirect(new URL('/login', req.nextUrl.origin));
+    }
+  } else {
+    if (AUTH_ROUTES.includes(pathName)) {
+      return NextResponse.redirect(new URL('/', req.nextUrl.origin));
+    }
+  }
+});
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
